bd1054d7e343d2b46a97a0876ec4ac1f510a4f0c
kde6260 <kde6260@gmail.com>
2019-12-08 22:30:32 +0900

    create Interget Gateway resource for public subnets

--- update-stack.sh
+++ update-stack.sh
@@ -1,5 +1,1 @@
-aws cloudformation update-stack
---stack-name $1
---template-body file://$2
---parameters file://$3
---profile=udacity-devops
+aws cloudformation update-stack --stack-name $1 --template-body file://$2 --parameters file://$3 --profile=udacity-devops
